7e3b2a26313911745bc1d7a3dc2f6179c6b4802c
trewos <n.samokhvalov@edu.internetdevels.com>
2023-06-01 15:11:10 +0300

    Finished layout building. Added adaptive for block`s. Added new functional using JS code.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Focus first button (portfolio block) */
+
 var buttons = document.getElementsByClassName('portfolio__button-element');
 var firstButton = buttons[0];
 
@@ -13,3 +15,63 @@ for (var i = 0; i < buttons.length; i++) {
         }
     });
 }
+
+/* Scroll link (header block) */
+
+
+function smoothScroll(event, target) {
+    event.preventDefault();
+    const element = document.getElementById(target);
+    const offsetTop = element.offsetTop;
+    const offsetHeight = element.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const distance = offsetTop - ((windowHeight - offsetHeight) / 2);
+    window.scroll({
+        top: offsetTop,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+/* Check input value (touch block) */
+
+const inputName = document.querySelector('.name');
+const inputEmail = document.querySelector('.email');
+const inputText = document.querySelector('.textarea');
+const submitButton = document.querySelector('.submit');
+
+submitButton.addEventListener('click', Error);
+
+function Error() {
+    const name = inputName.value;
+    const email = inputEmail.value;
+    const textarea = inputText.value;
+
+    const patternName = /^[A-Za-z]+$/;
+    const patternEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const patternText = /^[A-Za-z]+$/;
+
+    const isEmptyName = patternName.test(name);
+    const isEmptyEmail = patternEmail.test(email);
+    const isEmptyText = patternText.test(textarea);
+
+    inputName.style.border = isEmptyName ? '0px' : '1px solid red';
+    inputEmail.style.border = isEmptyEmail ? '0px' : '1px solid red';
+    inputText.style.border = isEmptyText ? '0px' : '1px solid red';
+}
+
+/* Delete padding with header */
+
+const wrapper = document.querySelector('.wrapper');
+const headers = wrapper.querySelectorAll('.header');
+const contents = wrapper.querySelectorAll('.content');
+
+function setPaddingToContents() {
+    contents.forEach((element) => {
+        element.style.padding = '0';
+    });
+}
+
+headers.forEach((header) => {
+    header.addEventListener('DOMNodeRemoved', setPaddingToContents);
+});
